7206f1a8e364a87b39b5d0bc300dd2291103de6a
LinexEPA <tc000013787@gmail.com>
2026-09-16 01:13:00 +0800

    Add lightweight submission token to learning style config

--- learning-style/config.js
+++ learning-style/config.js
@@ -1,2 +1,6 @@
 // Google Apps Script Web App 部署網址。
 window.LEARNING_STYLE_SUBMIT_URL = 'https://script.google.com/macros/s/AKfycbwoEhVZ4OxqlaJBSzcrxnWNmpyoCnhxUxjGXd5CJME_g_y0ZT8QQG3SYTbIh0gpaXxX/exec';
+
+// 簡易提交識別碼：用來阻擋非本測驗頁面的隨意 POST。
+// 注意：GitHub Pages 是公開前端，因此這不是機密憑證或完整驗證機制。
+window.LEARNING_STYLE_SUBMIT_TOKEN = 'xt5sNqkzln5pihFeAVq5jm1rvisnjnBM';
