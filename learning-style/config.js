74ad35706ca69092fdaabc6bc576e8bff49c23e0
LinexEPA <tc000013787@gmail.com>
2026-09-16 01:06:01 +0800

    Connect learning-style form to Apps Script endpoint

--- learning-style/config.js
+++ learning-style/config.js
@@ -1,3 +1,2 @@
-// Google Apps Script Web App 部署完成後，把網址貼在引號中。
-// 留空時，測驗與雷達圖仍可正常使用，只是不會寫入 Google Sheet。
-window.LEARNING_STYLE_SUBMIT_URL = '';
+// Google Apps Script Web App 部署網址。
+window.LEARNING_STYLE_SUBMIT_URL = 'https://script.google.com/macros/s/AKfycbwoEhVZ4OxqlaJBSzcrxnWNmpyoCnhxUxjGXd5CJME_g_y0ZT8QQG3SYTbIh0gpaXxX/exec';
